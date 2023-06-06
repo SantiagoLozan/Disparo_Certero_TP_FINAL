636b9c72ea4abdd0f0abeed401ac38de36cd6cc1
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2023-06-06 17:12:28 -0300

    Scenes

--- main.js
+++ main.js
@@ -33,15 +33,7 @@ const config = {
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [
-    Precarga,
-    Juego,
-    Informacion,
-    Ganador,
-    Perdedor,
-    MenuPrincipal,
-    HelloWorldScene,
-  ],
+  scene: [Precarga, Juego, Informacion, Ganador, Perdedor, MenuPrincipal],
 };
 
 // Create a new Phaser game instance
